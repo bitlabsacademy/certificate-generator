4080e173ce27c14d227b22d5356c634acf401193
syahrulhamdani <syahrulhamdani.ds@gmail.com>
2021-12-23 10:26:00 +0700

    feat: add certificate generator for webinar events

--- main.py
+++ main.py
@@ -4,28 +4,26 @@ import pandas as pd
 from PIL import Image, ImageColor, ImageDraw, ImageFont
 
 start = time()
-data_filename = "data/names/wds09201.csv"
-certif_filename = "data/certificates/wds09201.png"
-# names = ["Vincent Nelwan", "Achmad Maulana Dzaky"]
+data_filename = "data/webinar-certificates/dec/names/Claim Sertif DS.csv"
+certif_filename = "data/certificates/ds-template.png"
 names = pd.read_csv(
     data_filename,
-    # parse_dates=["Start Date (UTC)", "Submit Date (UTC)"]
+    parse_dates=["Submitted At"],
 )
-# names = names[names["Eligible for Sertif?"] == "Y"]
 names.sort_values("Submitted At", inplace=True)
-certid_prefix = "WDS09201{:03}"
+certid_prefix = "WDS1812{:03}"
 
 # set font type and color for name
 font_name = ImageFont.truetype(
-    "data/fonts/Epilogue/Epilogue-SemiBold.ttf",
-    size=2*22
+    "data/fonts/epilogue/epilogue-semi-bold.ttf",
+    size=22
 )
 color_name = ImageColor.getrgb("#192256")
 
 # set font type and color for certificate ID
 font_certid = ImageFont.truetype(
-    "data/fonts/Space_Mono/SpaceMono-Regular.ttf",
-    size=2*10
+    "data/fonts/space-mono/space-mono-regular.ttf",
+    size=10
 )
 color_certid = ImageColor.getrgb("#434B75")
 
@@ -36,7 +34,6 @@ image_width, image_height = certificate.size
 
 print("Generating {} certificates".format(names.shape[0]))
 for idx, name in enumerate(names["Nama Lengkap"]):
-# for idx, name in enumerate(names.head()):
     if (idx+1) % 50 == 0:
         print(f"Done generating {idx+1} certificates..")
     certificate = Image.open(certif_filename)
@@ -44,7 +41,6 @@ for idx, name in enumerate(names["Nama Lengkap"]):
 
     name_width, name_height = drawer.textsize(name.title(), font=font_name)
     certid_full = certid_prefix.format(idx+1)
-    # certid_full = certid_prefix
     certid_width, certid_height = drawer.textsize(
         certid_full, font=font_certid
     )
@@ -63,7 +59,7 @@ for idx, name in enumerate(names["Nama Lengkap"]):
         fill=color_certid
     )
     certificate.save(
-        f"data/webinar-certificates/sep/WDS09201/{certid_full}-{name.title()}.png"
+        f"data/webinar-certificates/december/WDS1812/{certid_full}-{name.title()}.png"
     )
 
 print("Done generating all {} certificates in {:.2f}s".format(
