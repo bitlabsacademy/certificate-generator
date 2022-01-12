2d0bbf73242bfd6e428538d9548792ed18a806c4
Syahrul Hamdani <syahrulhamdani.ds@gmail.com>
2022-01-12 15:19:07 +0700

    chore: use secrets

--- certificate/config.py
+++ certificate/config.py
@@ -1,11 +1,26 @@
 import os
 from pathlib import Path
 
+import streamlit as st
 
-IMAGE_TEMPLATE_DIR = Path(os.getenv("IMAGE_TEMPLATE_DIR", "templates"))
-FONT_DIR = Path(os.getenv("FONT_DIR", "fonts"))
-CONFIG_FILE = Path(os.getenv("CONFIG_FILE", "config"))
-OUT_DIR = Path(os.getenv("OUT_DIR", "outs"))
+
+# IMAGE_TEMPLATE_DIR = Path(os.getenv("IMAGE_TEMPLATE_DIR", "templates"))
+# FONT_DIR = Path(os.getenv("FONT_DIR", "fonts"))
+# CONFIG_FILE = Path(os.getenv("CONFIG_FILE", "config"))
+# OUT_DIR = Path(os.getenv("OUT_DIR", "outs"))
+
+IMAGE_TEMPLATE_DIR = Path(
+    os.getenv("IMAGE_TEMPLATE_DIR", st.secrets["IMAGE_TEMPLATE_DIR"])
+)
+FONT_DIR = Path(
+    os.getenv("FONT_DIR", st.secrets["FONT_DIR"])
+)
+CONFIG_FILE = Path(
+    os.getenv("CONFIG_FILE", st.secrets["CONFIG_FILE"])
+)
+OUT_DIR = Path(
+    os.getenv("OUT_DIR", st.secrets["OUT_DIR"])
+)
 
 
 DICT_CONTENT = {
