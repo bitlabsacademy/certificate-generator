0a14af3be063dc436a803358bf121e0ff0cd2aab
Syahrul Hamdani <syahrulhamdani.ds@gmail.com>
2022-01-12 15:03:02 +0700

    feat: add streamlit app

--- certificate/__init__.py
+++ certificate/__init__.py
@@ -1,27 +1,95 @@
 from dataclasses import dataclass, field
-from pathlib import Path
+from pathlib import Path, PosixPath
+from textwrap import wrap
+from time import time
 from typing import Any, Dict, List, Tuple
 
+import pandas as pd
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageFont, ImageColor, ImageDraw
 
 import certificate.config as c
 
 
 @dataclass
 class Certificate:
+    name: str = field(default_factory=str)
     template_image: str = "template"
     file_format: List[str] = field(
         default_factory=lambda: [".png", ".jpg"]
     )
 
-    def generate(self):
-        """Generate certificate based on content."""
-        pass
+    def _find_image(self):
+        for f in self.file_format:
+            filepath = c.IMAGE_TEMPLATE_DIR / (self.template_image + f)
+            if (filepath).exists():
+                return filepath
+        return
 
-    def save(self):
+    def _get_text(self, recipient, component: str, prefix: str = "") -> str:
+        if component == "id":
+            text = prefix + f"{recipient.Index+1:03}"
+            return text
+
+        attr = str(getattr(recipient, component))
+        return "\n".join(wrap(attr))
+
+    def _get_fonts(self, config: Dict, component: str) -> Dict:
+        font_type = ImageFont.truetype(
+            (
+                c.FONT_DIR
+                / (
+                    config[component + "_font_type"]
+                    + ".ttf"
+                )
+            ).as_posix(),
+            size=int(config[component + "_font_size"]),
+        )
+        font_color = ImageColor.getrgb(
+            config[component + "_font_color"]
+        )
+        return {"font_type": font_type, "font_color": font_color}
+
+    def generate(self, recipient, template, config: Dict,
+                 prefix: str = "WBL0101"):
+        """Generate certificate based on content.
+
+        Args:
+            recipient: file contains list of recipients as CSV file
+            config (Dict): configuration for writing the components
+            out_dir (str): directory the generated certificates are stored
+            prefix (str): prefix used when saving the certificates
+        """
+        template_config = config[self.template_image]
+        components = [
+            component.split("_", 1) for component in template_config.keys()
+        ]
+        drawer = ImageDraw.Draw(template)
+
+        for component in components:
+            text = self._get_text(recipient, component[0], prefix)
+            fonts = self._get_fonts(template_config, component[0])
+
+            drawer.textsize(text, font=fonts["font_type"])
+            drawer.text(
+                template_config[component[0] + "_loc"],
+                text=text,
+                anchor="la",
+                font=fonts["font_type"],
+                fill=fonts["font_color"]
+            )
+
+        setattr(self, "certificate_", template)
+        return template
+
+    def save(self, recipient, prefix: str = "WBL0101",
+             out_dir: PosixPath = Path()):
         """Save generated certificate."""
-        pass
+        out_dir.mkdir(exist_ok=True)
+        prefix_text = self._get_text(recipient, "id", prefix)
+        filename = (out_dir / (prefix_text + "_" + self.name)).as_posix()
+        self.certificate_.save(filename + ".png")
+        return
 
     @property
     def template(self):
@@ -33,7 +101,7 @@ class Certificate:
                 st.error(
                     "Cannot find any of these files: {}".format(
                         ", ".join([
-                            image_filepath.name + f
+                            img.name + f
                             for f in self.file_format
                         ])
                     )
@@ -42,9 +110,3 @@ class Certificate:
                 setattr(self, "_template", image)
                 return getattr(self, "_template")
 
-    def _find_image(self):
-        for f in self.file_format:
-            filepath = c.IMAGE_TEMPLATE_DIR / (self.template_image + f)
-            if (filepath).exists():
-                return filepath
-        return
--- certificate/config.py
+++ certificate/config.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 IMAGE_TEMPLATE_DIR = Path(os.getenv("IMAGE_TEMPLATE_DIR", "templates"))
 FONT_DIR = Path(os.getenv("FONT_DIR", "fonts"))
+CONFIG_FILE = Path(os.getenv("CONFIG_FILE", "config"))
+OUT_DIR = Path(os.getenv("OUT_DIR", "outs"))
 
 
 DICT_CONTENT = {
